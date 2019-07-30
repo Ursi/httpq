5a5e2515ab87ac6fc59d34ba6dac4dc69d49319b
Mason Mackaman <masondeanm@aol.com>
2019-07-29 20:34:36 -0400

    Total revamp
    http and https are included
    
    extended it to sense weather to use http or https instead of only being
    able to use https
    
    class of raw functions to get things other than just utf8 strings

--- httpq.js
+++ httpq.js
@@ -2,27 +2,43 @@ const http = require('http'),
 	https = require('https');
 
 module.exports = {
-	get(...params) {
+	async get(...params) {
+		return (await this.getRaw(...params)).toString();
+	},
+	async getRaw(...params) {
 		if (params.length > 1) {
 			params[1].method = 'GET';
 		}
 
-		return this.request(req => req.end(), ...params)
+		return (await this.request(req => req.end(), ...params));
 	},
-	post(message, options, encoding = 'utf8') {
+	http: http,
+	https: https,
+	async post(...params) {
+		return (await this.postRaw(...params)).toString();
+	},
+	async postRaw(message, options, encoding = 'utf8') {// encoding may have to be dealth with for sending raw data, not sure if it's ignored when sending a buffer
 		options.method = 'POST';
-		return this.request(req => req.end(message, encoding), options);
+		return (await this.request(req => req.end(message, encoding), options));
 	},
 	request(reqFunc, ...params) {
 		return new Promise((resolve, reject) => {
 			try {
-				let req = https.request.apply(https, [...params, res => {
-					res.setEncoding('utf8');
-					let body = '';
-					res.on('data', chunk => body += chunk);
-					res.on('end', ()=> resolve(body));
+				let protocol,
+					first = params[0];
+				if (typeof first == 'object') {
+					protocol = first.protocol;
+				} else {
+					protocol = /^https/.test(first) ? https : http;
+				}
+
+				let req = protocol.request.apply(protocol, [...params, res => {
+					let buffers = [];
+					res.on('data', data => buffers.push(data));
+					res.on('end', ()=> resolve(Buffer.concat(buffers)));
 				}]);
 
+				req.on('error', reject);
 				reqFunc(req);
 			} catch (error) {
 				reject(error);
@@ -30,19 +46,3 @@ module.exports = {
 		});
 	},
 };
-
-
-		/*let scheme;
-		if (/^https/.test(downloadLink)) {
-			scheme = https;
-		} else {
-			scheme = http
-		}
-
-		scheme.get(downloadLink, res => {
-			let buffArr = [];
-			res.on('data', data => buffArr.push(data));
-			res.on('end', ()=> resolve(fs.writeFileSync(path.join(videoPath, name + '.mp4'), Buffer.concat(buffArr))));
-		});
-
-*/
