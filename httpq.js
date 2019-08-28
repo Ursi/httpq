74cfe57d8e934575ccd3a33c49b1dab278fc09e8
Mason Mackaman <masondeanm@aol.com>
2019-08-28 00:57:06 -0400

    Add 'Res' and 'ToFile' methods

--- httpq.js
+++ httpq.js
@@ -1,50 +1,67 @@
-const http = require('http'),
-	https = require('https');
+const
+	fs = require(`fs`),
+	http = require(`http`),
+	https = require(`https`);
+
+function getProtocol(param) {
+	if (typeof param == `object`) {
+		return globalThis[param.protocol];
+	} else {
+		return /^https/.test(param) ? https : http;
+	}
+}
 
 module.exports = {
 	async get(...params) {
 		return (await this.getRaw(...params)).toString();
 	},
 	async getRaw(...params) {
+		return await this.resData(await this.getRes(...params));
+	},
+	async getRes(...params) {
 		if (params.length > 1) {
-			params[1].method = 'GET';
+			params[1].method = `GET`;
 		}
 
-		return (await this.request(req => req.end(), ...params));
+		return res = await this.request(req => req.end(), ...params);
+	},
+	getToFile(filePath, ...params) {
+		return new Promise(async resolve => {
+			(await this.getRes(...params)).pipe(fs.createWriteStream(filePath))
+				.on(`finish`, resolve);
+		});
 	},
 	http: http,
 	https: https,
 	async post(...params) {
 		return (await this.postRaw(...params)).toString();
 	},
-	async postRaw(message, options, encoding = 'utf8') {// encoding may have to be dealth with for sending raw data, not sure if it's ignored when sending a buffer
-		options.method = 'POST';
-		return (await this.request(req => req.end(message, encoding), options));
+	async postRaw(...params) {
+		return await this.resData(await this.postRes(...params));
+	},
+	async postRes(message, options, encoding = `utf8`) { // encoding may have to be dealth with for sending raw data, not sure if it's ignored when sending a buffer
+		options.method = `POST`;
+		return res = await this.request(req => req.end(message, encoding), options);
 	},
 	request(reqFunc, ...params) {
 		return new Promise((resolve, reject) => {
 			try {
-				let protocol,
-					first = params[0];
-				if (typeof first == 'object') {
-					protocol = first.protocol;
-				} else {
-					protocol = /^https/.test(first) ? https : http;
-				}
+				let
+					protocol = getProtocol(params[0]),
+					req = protocol.request(...params, res => resolve(res));
 
-				let req = protocol.request.apply(protocol, [...params, res => resolve(this.resData(res))]);
-				req.on('error', reject);
+				req.on(`error`, reject);
 				reqFunc(req);
 			} catch (error) {
 				reject(error);
 			}
 		});
 	},
-	resData(res) {
+	resData(res, write) {
 		return new Promise(resolve => {
 			let buffers = [];
-			res.on('data', data => buffers.push(data));
-			res.on('end', ()=> resolve(Buffer.concat(buffers)));
+			res.on(`data`, data => buffers.push(data));
+			res.on(`end`, ()=> resolve(Buffer.concat(buffers)));
 		});
 	},
 };
